89320937dc84efd1614cdb978c6a8b0c29d5e961
DoganHaluk <ha_look@yahoo.com>
2021-08-07 01:51:15 +0200

    fietsen application

--- src/test/java/be/vdab/fietsen/repositories/JpaDocentRepositoryTest.java
+++ src/test/java/be/vdab/fietsen/repositories/JpaDocentRepositoryTest.java
@@ -1,5 +1,6 @@
 package be.vdab.fietsen.repositories;
 
+import be.vdab.fietsen.domain.Geslacht;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -30,5 +31,19 @@ class JpaDocentRepositoryTest extends AbstractTransactionalJUnit4SpringContextTe
     @Test
     void findByOnbestaandeId() {
         assertThat(repository.findById(-1)).isNotPresent();
+    }
+
+    private long idVanTestVrouw() {
+        return jdbcTemplate.queryForObject("select id from docenten where voornaam = 'testV'", Long.class);
+    }
+
+    @Test
+    void man() {
+        assertThat(repository.findById(idVanTestMan())).hasValueSatisfying(docent -> assertThat(docent.getGeslacht()).isEqualTo(Geslacht.MAN));
+    }
+
+    @Test
+    void vrouw() {
+        assertThat(repository.findById(idVanTestVrouw())).hasValueSatisfying(docent -> assertThat(docent.getGeslacht()).isEqualTo(Geslacht.VROUW));
     }
 }
