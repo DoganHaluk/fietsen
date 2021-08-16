aa71cea52662c6e75c14cab2dfc78b19bd7d4e1e
DoganHaluk <ha_look@yahoo.com>
2021-08-16 23:39:54 +0200

    fietsen application

--- src/test/java/be/vdab/fietsen/services/DefaultDocentServiceIntegrationTest.java
+++ src/test/java/be/vdab/fietsen/services/DefaultDocentServiceIntegrationTest.java
@@ -1,6 +1,10 @@
 package be.vdab.fietsen.services;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 import javax.persistence.EntityManager;
@@ -8,6 +12,10 @@ import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DataJpaTest(showSql = false)
+@Import(DefaultDocentService.class)
+@ComponentScan(value = "be.vdab.fietsen.repositories", resourcePattern = "JpaDocentRepository.class")
+@Sql("/insertDocent.sql")
 public class DefaultDocentServiceIntegrationTest extends AbstractTransactionalJUnit4SpringContextTests {
     private final static String DOCENTEN = "docenten";
     private final DefaultDocentService service;
