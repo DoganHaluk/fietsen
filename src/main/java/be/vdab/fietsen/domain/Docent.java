445d76b7c78f3f727f0bbdaf9297bb1851517f52
DoganHaluk <ha_look@yahoo.com>
2021-10-19 00:32:04 +0200

    fietsen application

--- src/main/java/be/vdab/fietsen/domain/Docent.java
+++ src/main/java/be/vdab/fietsen/domain/Docent.java
@@ -10,7 +10,7 @@ import java.util.Set;
 @Entity
 @Table(name = "docenten")
 @NamedQuery(name = "Docent.findAll", query = "select d from Docent d order by d.wedde")
-@NamedEntityGraph(name = "Docent.metCampus", attributeNodes = @NamedAttributeNode("campus"))
+@NamedEntityGraph(name = Docent.MET_CAMPUS, attributeNodes = @NamedAttributeNode("campus"))
 public class Docent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
